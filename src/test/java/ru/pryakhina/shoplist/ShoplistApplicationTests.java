ba8464bf8d667101bcc931eeb124883bd55945ca
Elena (nvictor) <voshodsolnca@mail.ru>
2023-11-30 15:29:34 +0300

    add my project

---
+++ src/test/java/ru/pryakhina/shoplist/ShoplistApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pryakhina.shoplist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoplistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
