6319fd8ef8a9d24a9bb8376e6e33cadb5b7dff5a
Elena (nvictor) <voshodsolnca@mail.ru>
2023-12-03 16:30:00 +0300

    mod ShoplistApplication.java

--- src/main/java/ru/pryakhina/shoplist/ShoplistApplication.java
+++ src/main/java/ru/pryakhina/shoplist/ShoplistApplication.java
@@ -2,7 +2,10 @@ package ru.pryakhina.shoplist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+//@SpringBootApplication(scanBasePackages = "com.baeldung.boot.jsp")
 @SpringBootApplication
 public class ShoplistApplication {
 
